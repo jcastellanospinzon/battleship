490ddad3c1ad5158791949aebeb6af67b4e0aa6c
jcastellanospinzon <jcastellanospinzon@gmail.com>
2016-11-09 22:48:18 -0500

    Initial develop commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-repository
+# Battleship - The Game
+
+[![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](http://www.gnu.org/licenses/gpl-3.0)
+
+A pure Java Battleship game with sockets and swing ui.
---
+++ src/main/java/edu/udistrital/battleship/Launcher.java
@@ -0,0 +1,30 @@
+package edu.udistrital.battleship;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Launcher for the game.
+ *
+ * @author Julián Yezid Castellanos Pinzón <i>&lt;jcastellanospinzon@gmail.com&gt;</i>
+ */
+public class Launcher {
+
+    private static final Logger LOGGER = LogManager.getLogger(Launcher.class);
+
+    /**
+     * Launch the game.
+     *
+     * @param args Console arguments. Do nothing.
+     */
+    public static void main(String[] args) {
+        LOGGER.info("***************************");
+        LOGGER.info("** BATTLESHIP - THE GAME **");
+        LOGGER.info("***************************");
+        LOGGER.info("Starting...");
+
+
+        LOGGER.info("Started!");
+    }
+
+}
